a662f2d33f8a4aa38e0115b6cbc54143910f16c8
Ricardo N Feliciano <FelicianoTech@gmail.com>
2017-04-11 13:15:11 -0400

    Add Hugo 0.20.

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 0.19/Dockerfile -t felicianotech/docker-hugo:latest -t felicianotech/docker-hugo:0.19 .
+docker build --file 0.20/Dockerfile -t felicianotech/docker-hugo:latest -t felicianotech/docker-hugo:0.20 .
+docker build --file 0.19/Dockerfile -t felicianotech/docker-hugo:0.19 .
 docker build --file 0.18/Dockerfile -t felicianotech/docker-hugo:0.18.1  -t felicianotech/docker-hugo:0.18 .
