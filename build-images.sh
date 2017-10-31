4e5e09994171d59b84803204ed268af1bb9e29a4
Ricardo N Feliciano <FelicianoTech@gmail.com>
2017-10-31 14:05:04 -0400

    Update to Ruby 2.4.x and relayer Dockerfile template. (#25)

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 docker build --file 0.30/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.30.2  -t cibuilds/hugo:0.30 .
-docker build --file 0.29/Dockerfile -t cibuilds/hugo:0.29 .
-docker build --file 0.28/Dockerfile -t cibuilds/hugo:0.28 .
 docker build --file 0.30/Dockerfile -t felicianotech/docker-hugo:latest -t felicianotech/docker-hugo:0.30.2  -t felicianotech/docker-hugo:0.30 .
-docker build --file 0.29/Dockerfile -t felicianotech/docker-hugo:0.29 .
-docker build --file 0.28/Dockerfile -t felicianotech/docker-hugo:0.28 .
