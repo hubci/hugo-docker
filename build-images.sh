939be5bc2e4edc78667c088e2d9676444f177599
Ricardo N Feliciano <FelicianoTech@gmail.com>
2018-03-07 13:30:09 -0500

    Add Hugo v0.37.1.

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 0.37/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.37 .
-docker build --file 0.37/Dockerfile -t felicianotech/docker-hugo:latest -t felicianotech/docker-hugo:0.37 .
+docker build --file 0.37/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.37.1  -t cibuilds/hugo:0.37 .
+docker build --file 0.37/Dockerfile -t felicianotech/docker-hugo:latest -t felicianotech/docker-hugo:0.37.1  -t felicianotech/docker-hugo:0.37 .
