b678d515ceac75489dc75afb54fb1a2897c77120
Ricardo N Feliciano <FelicianoTech@gmail.com>
2018-10-29 08:38:29 -0400

    Create release script. [skip ci]

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+versions=( "$@" )
+
+sortedVersions=( $(
+	for version in "${versions[@]}"; do
+		echo "$version"
+	done | sort --reverse) )
+
+
+for i in "${!sortedVersions[@]}"; do
+
+	version=${sortedVersions[$i]}
+
+	git checkout -b hugo-v${version}
+	./gen-dockerfiles.sh ${version}
+	git add .
+	git commit -m "Add Hugo v${version}."
+	git push -u origin hugo-v${version}
+done
