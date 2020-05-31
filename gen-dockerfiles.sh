1d0c1ebf33186c9eb56939fdf690718649456e57
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-05-31 11:50:18 -0400

    Add support for hubci namespace.

--- gen-dockerfiles.sh
+++ gen-dockerfiles.sh
@@ -43,3 +43,42 @@ for i in "${!sortedVersions[@]}"; do
 
 	echo "$string" >> build-images.sh
 done
+
+
+
+repo="hubci/hugo:"
+
+# prepare file
+echo "" >> build-images.sh
+echo "" >> build-images.sh
+
+for i in "${!sortedVersions[@]}"; do
+	string="docker build"
+	version=${sortedVersions[$i]}
+
+	if [[ $version =~ ^[0-9]+\.[0-9]+ ]]; then
+		versionShort=${BASH_REMATCH[0]}
+	else
+		echo "Version matching failed." >&2
+		continue
+	fi
+
+	[[ -d "$versionShort" ]] || mkdir "$versionShort"
+
+	sed -r -e 's!%%HUGO_VERSION%%!'"$version"'!g' "Dockerfile.template" > "$versionShort/Dockerfile"
+
+	string="$string --file $versionShort/Dockerfile"
+	if [[ $i == 0 ]]; then
+		string="${string} -t ${repo}latest"
+	fi
+
+	string="${string} -t ${repo}${version}"
+
+	if [[ $versionShort != $version ]]; then
+		string="${string}  -t ${repo}${versionShort}"
+	fi
+
+	string="$string ."
+
+	echo "$string" >> build-images.sh
+done
