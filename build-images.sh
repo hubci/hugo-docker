87893f329e1bd2bf17e6a59365416966465f1cc6
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-12-19 12:24:06 -0500

    Add Hugo v0.79.1 [release]. (#196)
    * Add Hugo v0.79.1 [release].
    
    * Adding new HTML Proofer dep libffi-dev.

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 0.79/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.79.0  -t cibuilds/hugo:0.79 .
+docker build --file 0.79/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.79.1  -t cibuilds/hugo:0.79 .
 
 
-docker build --file 0.79/Dockerfile -t hubci/hugo:latest -t hubci/hugo:0.79.0  -t hubci/hugo:0.79 .
+docker build --file 0.79/Dockerfile -t hubci/hugo:latest -t hubci/hugo:0.79.1  -t hubci/hugo:0.79 .
