548ac43b165303b830c97fb3d4fd2a48222795fe
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-08-03 12:32:22 -0400

    Add Hugo v0.86.1 [release].

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 0.87/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.87.0  -t cibuilds/hugo:0.87 .
+docker build --file 0.86/Dockerfile -t cibuilds/hugo:latest -t cibuilds/hugo:0.86.1  -t cibuilds/hugo:0.86 .
 
 
-docker build --file 0.87/Dockerfile -t hubci/hugo:latest -t hubci/hugo:0.87.0  -t hubci/hugo:0.87 .
+docker build --file 0.86/Dockerfile -t hubci/hugo:latest -t hubci/hugo:0.86.1  -t hubci/hugo:0.86 .
