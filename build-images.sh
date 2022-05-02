b377aca878b468c9b6e03cdc09682f02b7922635
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-05-02 11:26:00 -0400

    Publish v0.98.0. [release] (#264)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 0.97/Dockerfile -t hubci/hugo:0.97.3  -t hubci/hugo:0.97 .
+docker build --file 0.98/Dockerfile -t hubci/hugo:0.98.0  -t hubci/hugo:0.98 .
