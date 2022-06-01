12f3c50ffceef4c79133d106fe28ddda59e3305f
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2022-06-01 13:41:35 -0400

    Publish v0.100.0. [release] (#269)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 0.99/Dockerfile -t hubci/hugo:0.99.1  -t hubci/hugo:0.99 .
+docker build --file 0.100/Dockerfile -t hubci/hugo:0.100.0  -t hubci/hugo:0.100 .
--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push hubci/hugo:0.99.1
-docker push hubci/hugo:0.99
+docker push hubci/hugo:0.100.0
+docker push hubci/hugo:0.100
