219156adeabe1b80386c382a85ae06f759fd4f3b
Marko Bevc <marko@scalefactory.com>
2023-03-13 14:43:26 +0000

    feat: Bump to 0.111.3

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 0.111/Dockerfile -t hubci/hugo:0.111.2 -t hubci/hugo:0.111 --platform linux/amd64 .
+docker build --file 0.111/Dockerfile -t hubci/hugo:0.111.3 -t hubci/hugo:0.111 --platform linux/amd64 .
