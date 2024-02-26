97b978b25e0bc253f5fccbf1b8c71a8c720c68c8
Marko Bevc <marko@scalefactory.com>
2024-02-26 20:14:17 +0000

    Publish v0.123.4. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 0.123/Dockerfile -t hubci/hugo:0.123.3 -t hubci/hugo:0.123 --platform linux/amd64 .
+docker build --file 0.123/Dockerfile -t hubci/hugo:0.123.4 -t hubci/hugo:0.123 --platform linux/amd64 .
