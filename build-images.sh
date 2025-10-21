c3302dc89ab7a426faa738a427e0075032e22df8
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2025-10-21 13:29:16 -0400

    Publish v0.152.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 0.151/Dockerfile -t hubci/hugo:0.151.2 -t hubci/hugo:0.151 --platform linux/amd64 .
+docker build --file 0.152/Dockerfile -t hubci/hugo:0.152.0 -t hubci/hugo:0.152 --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push hubci/hugo:0.151
-docker push hubci/hugo:0.151.2
+docker push hubci/hugo:0.152
+docker push hubci/hugo:0.152.0
