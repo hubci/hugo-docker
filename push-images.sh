2a5da795b3c876324d486a4f4918d6c258b0ef52
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2025-10-26 15:22:06 -0400

    Publish an edge v0.152.2 image. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push hubci/hugo:0.152
-docker push hubci/hugo:0.152.2
+docker push hubci/hugo:edge
